f4cfdb4103ec04a6e42753b7d73da3c3cd055538
Lozzarella <laurenbrooks70@gmail.com>
2022-06-02 22:41:39 +1000

    Chest

---
+++ Assets/Scripts/ChestHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestHandler : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            animator.SetTrigger("Open");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("Close");
+        }
+    }
+}
